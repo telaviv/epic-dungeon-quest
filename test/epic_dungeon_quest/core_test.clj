79efa548dd6eacf6181d78cb1cf2cccf1de28e99
Shawn Krisman <telaviv@github>
2013-12-25 01:49:09 -0800

    added lein project.

---
+++ test/epic_dungeon_quest/core_test.clj
@@ -0,0 +1,7 @@
+(ns epic-dungeon-quest.core-test
+  (:require [clojure.test :refer :all]
+            [epic-dungeon-quest.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
