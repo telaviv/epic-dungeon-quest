71abaf1a4db481cbcd2e2f45e93bd59d6d198f38
Shawn Krisman <telaviv@github>
2013-12-26 21:29:55 -0800

    changed license to gpl.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject epic-dungeon-quest "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :license {:name "GNU General Public License v3"
+            :url "http://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojure-lanterna "0.9.4"]])
