57148e607e71a5c49b30293236f124ba579aabdc
Shawn Krisman <telaviv@github>
2013-12-27 10:26:42 -0800

    corners are drawn for the card now.

--- src/epic_dungeon_quest/draw.clj
+++ src/epic_dungeon_quest/draw.clj
@@ -1,7 +1,11 @@
 (ns epic-dungeon-quest.draw)
 
 (defn create-sheet [w h]
-  (repeat h (repeat w " ")))
+  (vec (repeat h (vec (repeat w " ")))))
 
 (defn draw-card [card]
-  (create-sheet 16 12))
+  (-> (create-sheet 16 12)
+      (assoc-in [0 0] "╭")
+      (assoc-in [0 15] "╮")
+      (assoc-in [11 0] "╰")
+      (assoc-in [11 15] "╯")))
--- test/epic_dungeon_quest/draw_test.clj
+++ test/epic_dungeon_quest/draw_test.clj
@@ -6,8 +6,13 @@
 (defn height [buffer] (count buffer))
 
 (deftest test-draw-card
-  (testing "size should be 16x12"
-    (let [card {:name "Pickachu"}
-          buffer (draw-card card)]
+  (let [card {:name "Pickachu"}
+        buffer (draw-card card)]
+    (testing "size should be 16x12"
       (is (= 16 (width buffer))
-          (= 12 (height buffer))))))
+          (= 12 (height buffer))))
+    (testing "it should use unicode box corners."
+      (is (= "╭" (get-in buffer [0 0])))
+      (is (= "╮" (get-in buffer [0 15]))
+      (is (= "╰" (get-in buffer [11 0]))
+      (is (= "╯" (get-in buffer [11 15]))))))))
