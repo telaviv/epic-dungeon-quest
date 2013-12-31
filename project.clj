a35f3c38cc4f9c4b60d55616f36618313cbfb417
Shawn Krisman <telaviv@github>
2013-12-31 00:40:00 -0800

    now has a dependency on an updated fork of lanterna.

--- project.clj
+++ project.clj
@@ -4,6 +4,10 @@
   :license {:name "GNU General Public License v3"
             :url "http://www.gnu.org/licenses/gpl-3.0.txt"}
   :main epic-dungeon-quest.epic-dungeon-quest
+  :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
+  :git-dependencies [["https://github.com/telaviv/clojure-lanterna.git"
+                      "upgrade_lanterna_to_2.1.7"]]
+  :source-paths ["src" ".lein-git-deps/clojure-lanterna/src/"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [clojure-lanterna "0.9.4"]])
+                 [com.googlecode.lanterna/lanterna "2.1.7"]])
