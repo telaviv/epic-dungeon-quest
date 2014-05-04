7e4759ec6d836c0aa8c424d22051144602fbb872
Shawn Krisman <ssandboxx@hotmail.com>
2014-05-04 14:43:32 -0700

    input stream is used in the main.

--- src/epic_dungeon_quest/epic_dungeon_quest.clj
+++ src/epic_dungeon_quest/epic_dungeon_quest.clj
@@ -2,10 +2,12 @@
     (:require [epic-dungeon-quest.core :as epic]
               [epic-dungeon-quest.side :as side]
               [epic-dungeon-quest.draw :as draw]
+              [epic-dungeon-quest.input :as input]
               [lanterna.screen :as ls]))
 
 (defn -main []
   (let [screen (ls/get-screen :text)
+        input-stream (input/create-input-stream screen)
         enemy-side [(epic/spider-card)]
         player-side {:character (epic/character-card)
                      :attack [(epic/wooden-sword-card)]}
@@ -15,13 +17,13 @@
     (ls/put-sheet screen 0 0 (draw/draw-enemy-side enemy-side))
     (ls/put-sheet screen 0 18 (draw/draw-player-side player-side))
     (ls/redraw screen)
-    (ls/get-key-blocking screen)
+    (nth input-stream 0)
     (ls/put-sheet screen 0 0 (draw/draw-enemy-side enemy-side))
     (ls/put-sheet screen 0 18 (draw/draw-player-side attacked-player-side))
     (ls/redraw screen)
-    (ls/get-key-blocking screen)
+    (nth input-stream 1)
     (ls/put-sheet screen 0 0 (draw/draw-enemy-side attacked-enemy-side))
     (ls/put-sheet screen 0 18 (draw/draw-player-side attacked-player-side))
     (ls/redraw screen)
-    (ls/get-key-blocking screen)
+    (nth input-stream 2)
     (ls/stop screen)))
